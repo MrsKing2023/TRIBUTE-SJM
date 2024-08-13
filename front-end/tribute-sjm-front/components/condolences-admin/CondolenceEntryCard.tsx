d13dec3b68d51998ea1026e99f6f569c79a54d9b
MrsKing2023 <smarshall84@yahoo.com>
2024-08-13 17:15:58 -0400

    admin added update and edit ability

--- front-end/tribute-sjm-front/components/condolences-admin/CondolenceEntryCard.tsx
+++ front-end/tribute-sjm-front/components/condolences-admin/CondolenceEntryCard.tsx
@@ -1,7 +1,77 @@
+import { useState } from "react"
+
 export default function CondolenceEntryCard(props: any) {
 
+    const [entry, setEntry] =useState<boolean>(false);
+
+    const handleSubmit = (event: any) => {
+
+    }
+
     return (
-        <div className="border-2 p-2 m-2 bg-gray-100">
+        <div className="border-2 p-2 m-2 rounded-md bg-gray-100">
+            
+            {entry ?
+            
+            <form onSubmit={handleSubmit}>
+                <h1>Guest Book</h1>
+                <div>
+                    <h1>Entry:</h1>
+                    <textarea id="entry" name="entry" rows="5" cols="50" placeholder="What would you like to say about Susan?"></textarea>
+                </div>
+
+
+                <div>
+                    <h1>First Name:</h1>
+                    <input type="text" autoComplete="off" id="firstName" placeholder="First Name" required minLength={3} maxLength={40} />
+                </div>
+
+                <div>
+                    <h1>Last Name:</h1>
+                    <input type="text" autoComplete="off" id="lastName" placeholder="Last Name" required minLength={3} maxLength={40} />
+                </div>
+
+                <div>
+                    <h1 className="entryBox">Choose A Relationship</h1>
+                    <select defaultValue="default" id="relationship" name="relationship">
+                    <option value="default">Choose A Relationship</option>
+                    <option value="family">Family</option>
+                    <option value="friend">Friend</option>
+                    <option value="coworker">Coworker</option>
+                    <option value="classmate">Classmate</option>
+                    <option value="acquaintance">Acquaintance</option>
+                    <option value="daughter">Daughter</option>
+                    <option value="brother">Brother</option>
+                    <option value="niece">Niece</option>
+                    <option value="nephew">Nephew</option>
+                    <option value="neighbor">Neighbor</option>
+                    <option value="grandchild">Grandchild</option>
+                    <option value="son">Son</option>
+                    </select>
+                </div>
+
+                <div>
+                    <h1>Email Address:</h1>
+                    <input type="text" autoComplete="off" id="emailAddress" placeholder="Email Address" required minLength={3} maxLength={40} />
+                </div>
+                
+                <div>
+                    <h1>City:</h1>
+                    <input type="text" autoComplete="off" id="city" placeholder="City" required minLength={3} maxLength={40} />
+                </div>
+
+                <div>
+                    <h1>State:</h1>
+                    <input type="text" autoComplete="off" id="state" placeholder="State" required minLength={2} maxLength={40} />
+                </div>
+                <button className= "bg-green-500/80 p-2 rounded-md" type="submit">Submit</button>
+                <button className= "bg-red-500/80 p-2 rounded-md" onClick={() => setEntry(prev => !prev)}>Cancel</button>
+
+
+            </form>
+
+            :
+            <div>
             <p>Id: {props.entry.id}</p>
             <p>Entry: {props.entry.entry}</p>
             <p>First Name: {props.entry.firstName}</p>
@@ -11,6 +81,10 @@ export default function CondolenceEntryCard(props: any) {
             <p>City: {props.entry.city}</p>
             <p>State: {props.entry.state} </p>
             <button onClick={() => props.removeEntry(props.entry.id)}className="bg-red-500/80 mt-2 p-2 rounded-md">Delete</button>
+            <button onClick={() => setEntry(prev => !prev)} className="bg-green-500/80 mt-2 p-2 rounded-md">Edit Entry</button>
+            </div>
+
+            }
 
         </div>
     )
